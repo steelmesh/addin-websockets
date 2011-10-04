6a384a4ae7c8d32156f296ac397c051de8ba78b5
Damon Oehlman <damon.oehlman@gmail.com>
2011-10-04 15:49:09 +1000

    Added event handler

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 exports.install = function(mesh, instance) {
-    this.io = require('socket.io').listen(instance);
+    var io = this.io = require('socket.io').listen(instance);
+    
+    io.sockets.on('connection', function(socket) {
+        mesh.emit('socket', socket);
+        
+        socket.on('event', function() {
+            var emitArgs = ['event'].concat(Array.prototype.slice.call(arguments, 0)).concat('sock:' + socket.id);
+            
+            // send the event to other connected listeners
+            // and add the client id to the end of the list so it can be distinguished on the client
+            socket.broadcast.emit.apply(socket, emitArgs);
+        });
+    });
 };
